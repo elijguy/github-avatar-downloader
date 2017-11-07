bc404a0c1ff2fd0cdc3017581004c0f71419ce75
Elijah Guy <elijahguy17@yahoo.ca>
2017-11-07 01:56:07 +0000

    require to secret file added into download_avatars

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,22 @@
 var request = require('request');
-
+var token = require('./secrets');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
+
+   var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token'
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+
 
 }
 
@@ -12,3 +25,6 @@ getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
+
+
