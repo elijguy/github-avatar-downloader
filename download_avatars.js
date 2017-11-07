6732086384d681707817dea636f941f380eede64
Elijah Guy <elijahguy17@yahoo.ca>
2017-11-07 18:11:27 +0000

    made and added the downloadImageByURL function

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var token = require('./secrets');
+var fs = require('fs');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 
@@ -33,5 +34,27 @@ getRepoContributors("jquery", "jquery", function(err, result) {
 
 });
 
+function downloadImageByURL(url, filePath){
+
+var request = require('request');
+var fs = require('fs');
+
+request.get(url)
+       .on('error', function (err) {
+         throw err;
+       })
+       .on('response', function (response) {
+
+         console.log('Response Status Code: ', response.statusCode);
+         console.log('Response message:', response.statusMessage);
+         console.log('Content type:', response.headers['content-type']);
+
+       })
+       .on('end', function(){
+
+        console.log('download complete!');
+       })
+       .pipe(fs.createWriteStream(filePath));
 
 
+}
